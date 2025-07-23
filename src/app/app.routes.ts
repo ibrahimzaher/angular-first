4ccf27c18ac52351f07567bf11fb8e462347f34c
ibrahimzaher <hemazaher15@gmail.com>
2025-07-23 19:52:39 +0300

    added not-found component and config routing for app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./components/about/about').then((c) => c.About),
+  },
+  {
+    path: 'portofolio',
+    loadComponent: () =>
+      import('./components/portofolio/portofolio').then((c) => c.Portofolio),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./components/contact/contact').then((c) => c.Contact),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/not-found/not-found').then((c) => c.NotFound),
+  },
+];
