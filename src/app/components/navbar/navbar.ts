239b23ebd6815e65ff8f22c15825382617132829
ibrahimzaher <hemazaher15@gmail.com>
2025-07-23 23:39:46 +0300

    design navbar  and check routing working

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  styleUrl: './navbar.css',
 })
 export class Navbar {
-
+  isMobileShow: boolean = false;
+  isScrolled: boolean = false;
+  toggleShowMenuItemMobile() {
+    this.isMobileShow = !this.isMobileShow;
+  }
+  @HostListener('window:scroll', []) onScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
 }
