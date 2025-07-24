30502a4f3407b9ccbb8b641025eac84a572fa13b
ibrahimzaher <hemazaher15@gmail.com>
2025-07-24 18:28:27 +0300

    add meta title  for components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,26 +4,31 @@ import { Home } from './components/home/home';
 export const routes: Routes = [
   {
     path: '',
-    component: Home,
+    loadComponent: () => import('./components/home/home').then((c) => c.Home),
+    title: 'Home',
   },
   {
     path: 'about',
     loadComponent: () =>
       import('./components/about/about').then((c) => c.About),
+    title: 'About',
   },
   {
     path: 'portofolio',
     loadComponent: () =>
       import('./components/portofolio/portofolio').then((c) => c.Portofolio),
+    title: 'Portofolio',
   },
   {
     path: 'contact',
     loadComponent: () =>
       import('./components/contact/contact').then((c) => c.Contact),
+    title: 'Contact',
   },
   {
     path: '**',
     loadComponent: () =>
       import('./components/not-found/not-found').then((c) => c.NotFound),
+    title: 'Not Found',
   },
 ];
